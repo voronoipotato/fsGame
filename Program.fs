37128c1829642afbd6643b22b569f07c7500cef3
alan ball <alanball@localhost.localdomain>
2019-06-02 12:20:20 -0400

    initial commit

---
+++ Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Veldrid.StartupUtilities
+
+module Game = 
+    let createWindow title = 
+        let mutable wci = WindowCreateInfo()
+        wci.X <- 100
+        wci.Y <- 100
+        wci.WindowWidth <- 960
+        wci.WindowHeight <- 540
+        wci.WindowTitle <- title
+        wci
+
+[<EntryPoint>]
+let main argv =
+    
+    let wci = Game.createWindow "Suits"
+    
+    let window = VeldridStartup.CreateWindow(ref wci)
+    let graphicsDevice = VeldridStartup.CreateGraphicsDevice(window)
+    while window.Exists do
+        window.PumpEvents() |> ignore
+    0 // return an integer exit code
