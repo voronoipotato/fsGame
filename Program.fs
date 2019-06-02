18feead6c48b1faa8dd9dd73d9da11ca0978051e
Alan Ball <vash.arclan@gmail.com>
2019-06-02 14:43:48 -0400

    added graphics buffers

--- Program.fs
+++ Program.fs
@@ -2,8 +2,22 @@
 
 open System
 open Veldrid.StartupUtilities
+open System.Numerics
+open Veldrid
+open Veldrid
+open Veldrid
+open Veldrid
+open Veldrid
+open Veldrid
+open Veldrid
+open Veldrid
 
 module Game = 
+    [<Struct>]
+    type VertexPositionColor = 
+        {Position: Vector2; Color: RgbaFloat}
+        static member VertexPositionColor(position, color) = {Position = position; Color = color}
+
     let createWindow title = 
         let mutable wci = WindowCreateInfo()
         wci.X <- 100
@@ -13,13 +27,34 @@ module Game =
         wci.WindowTitle <- title
         wci
 
+    let createResources window = 
+        let graphicsDevice = VeldridStartup.CreateGraphicsDevice(window)
+        let factory = graphicsDevice.ResourceFactory
+        let quadVerticies' = [|
+            Vector2(-0.75f,0.75f), RgbaFloat.Red
+            Vector2(0.75f, 0.75f), RgbaFloat.Green 
+            Vector2(-0.75f,-0.75f), RgbaFloat.Blue
+            Vector2(0.75f,-0.75f), RgbaFloat.Yellow 
+            |]
+        let quadVerticies = quadVerticies' |> Array.map VertexPositionColor.VertexPositionColor
+        let quadIndicies : uint16[] = [|0us;1us;2us;3us|]
+        let quadVerticiesSize : uint32 = sizeof<VertexPositionColor> * quadVerticies.Length |> uint32
+        let vertexBuffer = factory.CreateBuffer(BufferDescription(quadVerticiesSize,BufferUsage.VertexBuffer))
+        let indiciesSize = 4u * uint32 sizeof<uint16>
+        let indexBuffer = factory.CreateBuffer(BufferDescription(indiciesSize, BufferUsage.IndexBuffer))
+        graphicsDevice.UpdateBuffer (vertexBuffer, 0u , quadVerticies) |> ignore
+        graphicsDevice.UpdateBuffer (indexBuffer, 0u, quadIndicies ) |> ignore
+        ()
+        
+        
+
 [<EntryPoint>]
 let main argv =
     
     let wci = Game.createWindow "Suits"
     
     let window = VeldridStartup.CreateWindow(ref wci)
-    let graphicsDevice = VeldridStartup.CreateGraphicsDevice(window)
+    Game.createResources window |> ignore
     while window.Exists do
         window.PumpEvents() |> ignore
     0 // return an integer exit code
